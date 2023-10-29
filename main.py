5870486df8d206265a0f93d2b2d918646d57e146
AndersSaints <anders.lsantos@gmail.com>
2023-10-29 01:10:46 +0000

    Finished

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 import tkinter as tk
 import pandas as pd
 import random
@@ -23,8 +24,7 @@ def check_answer(answer):
     if current_question < len(questions):
         next_question()
     else:
-        pass
-        #show_result()
+        show_result()
 
 
 def next_question():
@@ -36,6 +36,25 @@ def next_question():
     option4_btn.config(text=option4, state=tk.NORMAL, command=lambda:check_answer(4))
 
     correct_answer.set(answer)
+
+def show_result():
+    messagebox.showinfo("Quiz completed", f"Congrats! You have completed the quiz. \n\nFinal score: {score}/{len(questions)}")
+    option1_btn.config(state=tk.DISABLED)
+    option2_btn.config(state=tk.DISABLED)
+    option3_btn.config(state=tk.DISABLED)
+    option4_btn.config(state=tk.DISABLED)
+    play_again_btn.pack()
+
+def play_again():
+    global score, current_question
+    score = 0
+    current_question = 0
+    random.shuffle(questions)
+    option1_btn.config(state=tk.NORMAL)
+    option2_btn.config(state=tk.NORMAL)
+    option3_btn.config(state=tk.NORMAL)
+    option4_btn.config(state=tk.NORMAL)
+    play_again_btn.pack_forget()
 
 
 
@@ -56,25 +75,25 @@ app_label = tk.Label(window, image=app_icon, bg=bg_color)
 app_label.pack(pady=10)
 
 # interface components
-question_label = tk.Label(window, text="Question", wraplength=380, bg=bg_color, fg=text_color, font=("Arial", 12, "bold"))
+question_label = tk.Label(window, text="Question", wraplength=380, bg=bg_color, fg=text_color,  font=("Arial", 12, "bold"))
 question_label.pack(pady=20)
 
 correct_answer = tk.IntVar()
 
-option1_btn = tk.Button(window, text="Option 1", width=30, bg=button_color, fg=button_text_color, font=("Arial", 10, "bold"))
+option1_btn = tk.Button(window, text="Option 1", width=30, bg=button_color, fg=button_text_color, state=tk.DISABLED, font=("Arial", 10, "bold"))
 option1_btn.pack(pady=10)
 
-option2_btn = tk.Button(window, text="Option 2", width=30, bg=button_color, fg=button_text_color, font=("Arial", 10, "bold"))
+option2_btn = tk.Button(window, text="Option 2", width=30, bg=button_color, fg=button_text_color, state=tk.DISABLED, font=("Arial", 10, "bold"))
 option2_btn.pack(pady=10)
 
-option3_btn = tk.Button(window, text="Option 3", width=30, bg=button_color, fg=button_text_color, font=("Arial", 10, "bold"))
+option3_btn = tk.Button(window, text="Option 3", width=30, bg=button_color, fg=button_text_color, state=tk.DISABLED, font=("Arial", 10, "bold"))
 option3_btn.pack(pady=10)
 
-option4_btn = tk.Button(window, text="Option 4", width=30, bg=button_color, fg=button_text_color, font=("Arial", 10, "bold"))
+option4_btn = tk.Button(window, text="Option 4", width=30, bg=button_color, fg=button_text_color, state=tk.DISABLED, font=("Arial", 10, "bold"))
 option4_btn.pack(pady=10)
 
-play_again_btn = tk.Button(window, text="Play again!", width=30, bg=button_color, fg=button_text_color, font=("Arial", 10, "bold"))
-#play_again_btn.pack(pady=20)
+play_again_btn = tk.Button(window, command=play_again, text="Play again!", width=30, bg=button_color, fg=button_text_color, font=("Arial", 10, "bold"))
+
 
 
 next_question()
